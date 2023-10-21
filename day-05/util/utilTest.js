a2fa6e56eb07f0c6e14ca5881fc2916fa94d0f5c
Dierk Koenig <dierk.koenig@canoo.com>
2023-10-21 10:49:19 +0200

    day 6 test function extracted

--- day-05/util/utilTest.js
+++ day-05/util/utilTest.js
@@ -1,6 +1,23 @@
 
 // requires util.js
 
+const test = (name, callback) => {
+    // pre
+    const ok = [];
+    // work
+    callback(ok);
+    // post
+    report(name, ok);
+};
+
+test ("util-times-new", ok => {
+    const collect = [];
+    (10).times( n => collect.push(n) );
+    ok.push(collect.length === 11);
+    ok.push(collect[0] === 0);
+    ok.push(collect[9] === 9);
+});
+
 
 // extending the prototype of many objects
 ( () => {
@@ -11,7 +28,7 @@
 
     (10).times( n => collect.push(n) );
 
-    ok.push(collect.length === 10);
+    ok.push(collect.length === 11);
     ok.push(collect[0] === 0);
     ok.push(collect[9] === 9);
 
